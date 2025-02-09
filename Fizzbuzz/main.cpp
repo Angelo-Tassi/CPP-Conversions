d5e7bf60dafce1a90d8e4ce38fad1bf3dda459ad
Angelo Tassi <angelotax@gmail.com>
2025-02-09 17:19:50 +0100

    Converted

--- Fizzbuzz/main.cpp
+++ Fizzbuzz/main.cpp
@@ -14,3 +14,22 @@ int main() {
     }
     return 0;
 }
+
+//Python to convert
+
+// for number in range(1, 101):
+//
+//     if number % 3 == 0 and number % 5 == 0:
+//
+//         print("FizzBuzz")
+//
+//     elif number % 3 == 0:
+//
+//         print("Fizz")
+//
+//     elif number % 5 == 0:
+//
+//         print("Buzz")
+//
+//     else:
+//         print(number)
